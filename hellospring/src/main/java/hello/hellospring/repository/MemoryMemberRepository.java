d0f53eb3bc477c4cbc12a13e5e9722f88d67d997
minko <minye0812@naver.com>
2024-04-11 17:13:37 +0900

    feat: 프로젝트 생성

---
+++ hellospring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,34 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L; //0,1,2 등 key값 생성
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence); //id값 세팅
+        store.put(member.getId(), member); //store에 저장
+        return member;
+    }
+
+    @Override
+    public Optional<Member> FindById(Long id) {
+        return Optional.ofNullable(store.get(id));
+        //get(id)가 null이여도, optional로 감싼다
+    }
+
+    @Override
+    public Optional<Member> FindByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
